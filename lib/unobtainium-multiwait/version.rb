ad8b6e41a0a41b6daa4546f0039654da5c5863c1
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-04-09 12:45:45 +0200

    Bump own version

--- lib/unobtainium-multiwait/version.rb
+++ lib/unobtainium-multiwait/version.rb
@@ -3,12 +3,12 @@
 # unobtainium-multiwait
 # https://github.com/jfinkhaeuser/unobtainium-multiwait
 #
-# Copyright (c) 2016 Jens Finkhaeuser and other unobtainium-multiwait contributors.
+# Copyright (c) 2016-2018 Jens Finkhaeuser and other unobtainium-multiwait contributors.
 # All rights reserved.
 #
 module Unobtainium
   module MultiWait
     # The current release version
-    VERSION = "0.2.0".freeze
+    VERSION = "0.3.0".freeze
   end # module MultiWait
 end # module Unobtainium
